555212fc8c42c7b5ef74d7878252a864640f8c66
hiromi.masuda <hiromi.c.masuda@rakuten.com>
2018-06-12 03:42:33 +0000

    user registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -21,13 +21,15 @@ class RegisterController extends Controller
     */
 
     use RegistersUsers;
+    
+    
 
     /**
      * Where to redirect users after registration.
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -36,7 +38,7 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest');
+        $this->middleware('guest')->except('logout');
     }
 
     /**
