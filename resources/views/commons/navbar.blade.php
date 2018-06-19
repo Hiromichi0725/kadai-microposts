852aafd348611adafdcc699855ca7c8d0c3271f4
hiromi.masuda <hiromi.c.masuda@rakuten.com>
2018-06-19 05:07:23 +0000

    follow, unfollow

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -9,6 +9,7 @@
                     <span class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand" href="/">Microposts</a>
+                
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
